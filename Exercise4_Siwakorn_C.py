63cbfa576fd0fdcacd7e73f5b323c29f278a817a
siwakorn07 <65013223+siwakorn07@users.noreply.github.com>
2020-05-11 14:15:45 +0700

    Delete Exercise4_Siwakorn_C.py

--- Exercise4_Siwakorn_C.py
+++
@@ -1,9 +0,0 @@
-FE = 85.74
-GB = 72.39
-ICS = 79.51
-CP = 96.87
-print("--- Your Score ---")
-print("Foundation English :",FE)
-print("General Business :",GB)
-print("Introduction to Computer Systems :",ICS)
-print("Computer Programing :",CP)
