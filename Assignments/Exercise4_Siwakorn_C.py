efd30858e4d07fbaae500c2c6fea9d117a15faaf
siwakorn07 <65013223+siwakorn07@users.noreply.github.com>
2020-05-11 14:14:34 +0700

    Upload Exercise4

---
+++ Assignments/Exercise4_Siwakorn_C.py
@@ -0,0 +1,9 @@
+FE = 85.74
+GB = 72.39
+ICS = 79.51
+CP = 96.87
+print("--- Your Score ---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programing :",CP)
