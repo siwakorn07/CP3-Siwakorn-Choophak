ca2ebef38088009d42f86ef191b1b06938b85d7c
siwakorn <time.siwakorn@gmail.com>
2020-05-22 14:15:45 +0700

    Create Lecture50_Siwakorn_C.py

---
+++ Assignments/Lecture50_Siwakorn_C.py
@@ -0,0 +1,14 @@
+def addNumber(x,y):
+    print(x+y)
+def subtractNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+num1 = int(input("x : "))
+num2 = int(input("y : "))
+addNumber(num1,num2)
+subtractNumber(num1,num2)
+multiplyNumber(num1,num2)
+divideNumber(num1,num2)
